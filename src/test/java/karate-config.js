7876bc130a51e352fe9c11d96ce519c612be84f7
jalonso <jalonso@stratio.com>
2023-11-13 10:12:29 +0100

    First configuration

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,20 +1,45 @@
 function fn() {
     var config = {
         name: 'Stratio',
-        baseURL: 'https://reqres.in/api'
     };
 
     var env = karate.env;
+    if (!env) {
+        env = 'dev';
+    }
+
     karate.log('Given Env: ', env);
     if (env == 'dev') {
         config.baseURL = 'https://reqres.in/dev/api'
-    } else if (env == 'prod') {
-        config.baseURL = 'https://reqres.in/prod/api'
-    } else if (env == 'local') {
-        config.baseURL = 'https://reqres.in/api'
+        config.sso_url = "https://sso-pre.int.stratio.com/login"
     }
+
+    var browser = karate.properties['browser'] || 'firefox';
+    karate.log('the browser set is: ' + browser + ', default: "chrome"');
+  
+    var grid_url = karate.properties['grid_url'] || false;
+    karate.log('the grid url set is: ' + grid_url + ', default: false');
+
+    if (browser == 'chrome') {
+        if (!grid_url) {
+           karate.configure('driver', { type: 'chrome', addOptions: ["--remote-allow-origins=*"] });
+           karate.log("Selected Chrome");
+        } else {
+           karate.configure('driver', { type: 'chrome', start: false, webDriverUrl: grid_url });
+           karate.log("Selected Chrome in grid");
+        }
+    } else if (browser == 'firefox') {
+        if (!grid_url) {
+            karate.configure('driver', { type: 'geckodriver' });
+            karate.log("Selected Firefox");
+        } else {
+            karate.configure('driver', { type: 'geckodriver', start: false, webDriverUrl: grid_url });
+            karate.log("Selected Firefox in grid");
+        }
+    } 
 
     karate.configure('connectTimeout', 5000);
     karate.configure('readTimeout', 5000);
+
     return config;
 }
